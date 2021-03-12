0ac8e3de5ac9b4d6285f3921a2a70352e853120c
Will Garside <worgarside@gmail.com>
2021-03-12 15:02:27 +0000

    blackened

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
-from base64 import b64decode
 from html import unescape
 from io import BytesIO
-from json import loads
 from os import getenv
+from os.path import join, abspath, dirname
 from tkinter import *
 from tkinter.font import Font
-from traceback import format_exc
-from requests import get
+
 from PIL import Image, ImageTk
 from dotenv import load_dotenv
-from requests import post
-from datetime import datetime, timedelta
-from json import load, dumps, dump
-from logging import StreamHandler, FileHandler, Formatter, getLogger, DEBUG
-from os import getenv, mkdir
-from os.path import join, abspath, dirname, sep
-from pathlib import Path
-
-from dotenv import load_dotenv
+from requests import get
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
-from sys import stdout
 
 load_dotenv(join(abspath(dirname(__file__)), ".env"))
 
@@ -28,14 +17,9 @@ load_dotenv(join(abspath(dirname(__file__)), ".env"))
 # Constants #
 #############
 
-# PB_PARAMS = {
-#     'token': getenv('PB_API_KEY'),
-#     't': 'CRT MQTT Alert'
-# }
-# SPEAKERS = ['media_player.all_speakers', 'media_player.hifi_system']
-CRT_PIN = int(getenv('CRT_PIN'))
-BG_COLOR = '#000000'
-STANDARD_ARGS = {'highlightthickness': 0, 'bd': 0, 'bg': BG_COLOR}
+CRT_PIN = int(getenv("CRT_PIN"))
+BG_COLOR = "#000000"
+STANDARD_ARGS = {"highlightthickness": 0, "bd": 0, "bg": BG_COLOR}
 CHAR_LIM = 31
 
 ALL_SCOPES = [
@@ -84,55 +68,49 @@ try:
     pi = rasp_pi()
     pi.set_mode(CRT_PIN, OUTPUT)
 
-
     def switch_on():
         pi.write(CRT_PIN, True)
 
-
     def switch_off():
         pi.write(CRT_PIN, False)
+
+
 except (AttributeError, ModuleNotFoundError):
+
     def switch_on():
         pass
 
-
     def switch_off():
         pass
-
-
-def pb_notify(m, t, token):
-    post(
-        'https://api.pushbullet.com/v2/pushes',
-        headers={
-            'Access-Token': token,
-            'Content-Type': 'application/json'
-        },
-        json={
-            'body': m,
-            'title': t,
-            'type': 'note'
-        }
-    )
 
 
 def update_display(payload):
     global content_dict
 
-    content_dict['images']['tk_img'] = Image.open(BytesIO(payload['attributes']['artwork']))
+    content_dict["images"]["tk_img"] = Image.open(
+        BytesIO(payload["attributes"]["artwork"])
+    )
 
-    content_dict['images']['tk_img'] = content_dict['images']['tk_img'].resize((image_size, image_size),
-                                                                               Image.ANTIALIAS)
-    content_dict['images']['artwork'] = ImageTk.PhotoImage(content_dict['images']['tk_img'])
+    content_dict["images"]["tk_img"] = content_dict["images"]["tk_img"].resize(
+        (image_size, image_size), Image.ANTIALIAS
+    )
+    content_dict["images"]["artwork"] = ImageTk.PhotoImage(
+        content_dict["images"]["tk_img"]
+    )
 
-    content_dict['widgets']['artwork'].configure(image=content_dict['images']['artwork'])
+    content_dict["widgets"]["artwork"].configure(
+        image=content_dict["images"]["artwork"]
+    )
 
-    del payload['attributes']['artwork']
+    del payload["attributes"]["artwork"]
 
-    for k, v in payload['attributes'].items():
-        if k in content_dict['widgets']:
-            content_dict['widgets'][k].config(text=unescape(v))
-            if len(content_dict['widgets'][k]['text']) > CHAR_LIM:
-                content_dict['widgets'][k]['text'] = ('  ' + content_dict['widgets'][k]['text'] + '  ') * 3
+    for k, v in payload["attributes"].items():
+        if k in content_dict["widgets"]:
+            content_dict["widgets"][k].config(text=unescape(v))
+            if len(content_dict["widgets"][k]["text"]) > CHAR_LIM:
+                content_dict["widgets"][k]["text"] = (
+                    "  " + content_dict["widgets"][k]["text"] + "  "
+                ) * 3
 
                 hscroll_label(k)
 
@@ -142,40 +120,38 @@ def update_display(payload):
 def hscroll_label(k):
     global content_dict
 
-    content_dict['coords'][k]['x'] -= 2
+    content_dict["coords"][k]["x"] -= 2
 
-    content_dict['coords'][k]['x'] = 0.5 * dims[0] \
-        if content_dict['coords'][k]['x'] < (0.5 * dims[0]) - (content_dict['widgets'][k].winfo_width() / 3) \
-        else content_dict['coords'][k]['x']
+    content_dict["coords"][k]["x"] = (
+        0.5 * dims[0]
+        if content_dict["coords"][k]["x"]
+        < (0.5 * dims[0]) - (content_dict["widgets"][k].winfo_width() / 3)
+        else content_dict["coords"][k]["x"]
+    )
 
-    content_dict['widgets'][k].place(**content_dict['coords'][k])
+    content_dict["widgets"][k].place(**content_dict["coords"][k])
 
-    if len(content_dict['widgets'][k]['text']) > CHAR_LIM:
-        content_dict['widgets']['canvas'].after(10, hscroll_label, k)
+    if len(content_dict["widgets"][k]["text"]) > CHAR_LIM:
+        content_dict["widgets"]["canvas"].after(10, hscroll_label, k)
     else:
-        content_dict['coords'][k]['x'] = 0.5 * dims[0]
-        content_dict['widgets'][k].place(**content_dict['coords'][k])
+        content_dict["coords"][k]["x"] = 0.5 * dims[0]
+        content_dict["widgets"][k].place(**content_dict["coords"][k])
 
 
 def execute_command(payload):
-    command_dict = {
-        'switch_on': switch_on,
-        'switch_off': switch_off
-    }
+    command_dict = {"switch_on": switch_on, "switch_off": switch_off}
 
-    if payload['attributes']['command'] in command_dict:
-        command_dict[payload['attributes']['command']]()
+    if payload["attributes"]["command"] in command_dict:
+        command_dict[payload["attributes"]["command"]]()
     else:
-        raise ValueError('Command not found')
+        raise ValueError("Command not found")
 
 
 def get_update():
     res = SPOTIFY.current_user_playing_track()
 
     for img in sorted(
-        res["item"]["album"]["images"],
-        key=lambda i: i["height"],
-        reverse=True
+        res["item"]["album"]["images"], key=lambda i: i["height"], reverse=True
     ):
         if img["height"] >= 300:
             artwork_url = img["url"]
@@ -187,13 +163,14 @@ def get_update():
         "attributes": {
             "artwork": get(artwork_url).content,
             "media_title": res.get("item", {}).get("album", {}).get("name", "?"),
-            "media_artist":  ", ".join(artist.get("name", "?") for artist in res.get("item", {}).get("album", {}).get("artists", []))
+            "media_artist": ", ".join(
+                artist.get("name", "?")
+                for artist in res.get("item", {}).get("album", {}).get("artists", [])
+            ),
         }
     }
 
     update_display(payload)
-
-
 
 
 def initialize():
@@ -202,60 +179,61 @@ def initialize():
     switch_on()
 
     root = Tk()
-    root.attributes('-fullscreen', True)
+    root.attributes("-fullscreen", True)
     root.configure(bg=BG_COLOR)
 
     dims = root.winfo_screenwidth(), root.winfo_screenheight()
 
     image_size = int(0.65 * dims[1])
 
-    crt_font = Font(family='Courier New', size=int(0.05 * dims[1]))
+    crt_font = Font(family="Courier New", size=int(0.05 * dims[1]))
 
     content_dict = {
-        'images': {
-            'tk_img': None,
-            'artwork': ''
+        "images": {"tk_img": None, "artwork": ""},
+        "widgets": {
+            "canvas": Canvas(root, width=dims[0], height=dims[1], **STANDARD_ARGS)
         },
-        'widgets': {
-            'canvas': Canvas(root, width=dims[0], height=dims[1], **STANDARD_ARGS)
+        "coords": {
+            "media_title": {"x": 0.5 * dims[0], "y": 0.8 * dims[1], "anchor": CENTER},
+            "media_artist": {"x": 0.5 * dims[0], "y": 0.9 * dims[1], "anchor": CENTER},
         },
-        'coords': {
-            'media_title': {'x': 0.5 * dims[0], 'y': 0.8 * dims[1], 'anchor': CENTER},
-            'media_artist': {'x': 0.5 * dims[0], 'y': 0.9 * dims[1], 'anchor': CENTER}
-        }
     }
-    content_dict['widgets']['canvas'].place(x=0, y=0, width=dims[0], height=dims[1])
+    content_dict["widgets"]["canvas"].place(x=0, y=0, width=dims[0], height=dims[1])
 
-    content_dict['widgets']['artwork'] = Label(
-        content_dict['widgets']['canvas'],
-        image='',
-        **STANDARD_ARGS
+    content_dict["widgets"]["artwork"] = Label(
+        content_dict["widgets"]["canvas"], image="", **STANDARD_ARGS
     )
 
-    content_dict['widgets']['media_title'] = Label(
-        content_dict['widgets']['canvas'],
-        text='',
+    content_dict["widgets"]["media_title"] = Label(
+        content_dict["widgets"]["canvas"],
+        text="",
         font=crt_font,
-        fg='#ffffff',
-        bg=BG_COLOR
+        fg="#ffffff",
+        bg=BG_COLOR,
     )
 
-    content_dict['widgets']['media_artist'] = Label(
-        content_dict['widgets']['canvas'],
-        text='',
+    content_dict["widgets"]["media_artist"] = Label(
+        content_dict["widgets"]["canvas"],
+        text="",
         font=crt_font,
-        fg='#ffffff',
-        bg=BG_COLOR
+        fg="#ffffff",
+        bg=BG_COLOR,
     )
 
-    content_dict['widgets']['artwork'].place(x=0.5 * dims[0], y=(0.5 * image_size) + (0.075 * dims[1]), anchor=CENTER)
-    content_dict['widgets']['media_title'].place(**content_dict['coords']['media_title'])
-    content_dict['widgets']['media_artist'].place(**content_dict['coords']['media_artist'])
+    content_dict["widgets"]["artwork"].place(
+        x=0.5 * dims[0], y=(0.5 * image_size) + (0.075 * dims[1]), anchor=CENTER
+    )
+    content_dict["widgets"]["media_title"].place(
+        **content_dict["coords"]["media_title"]
+    )
+    content_dict["widgets"]["media_artist"].place(
+        **content_dict["coords"]["media_artist"]
+    )
 
     return root
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     tk_root = initialize()
     tk_root.after(1000, func=get_update)
     tk_root.mainloop()
